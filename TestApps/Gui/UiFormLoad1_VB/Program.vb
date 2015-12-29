e09081bb6daa47d3069ec453572f0bf62e4f0248
ric <garlicbready@googlemail.com>
2015-12-29 14:44:54 +0000

    additon of more code

--- TestApps/Gui/UiFormLoad1_VB/Program.vb
+++ TestApps/Gui/UiFormLoad1_VB/Program.vb
@@ -28,9 +28,46 @@ Public Class Program
         ' This might be because the function calls in C++ use templates (generics) to specify the type
         'QWidget.Qt_qFindChild_helper(widg1, "pushButton", )
 
+        ' Looks like we'll need to re-implement our own findchild / find children method
+        ' using QWidget.Qt_qFindChild_helper
+        ' see C:\Qt\Qt5.5.1\5.5\Src\qtbase\src\corelib\kernel\qobject.h
+        Dim test1 = findChild(Of QPushButton)(widg1,"pushButton")
+
         ' Run the QApplication Process
         QApplication.Exec()
 
     End Sub
+
+
+    Public shared Function findChild(of T As QObject)(parent As QObject, name As string, _
+            Optional options As Qt.FindChildOption = Qt.FindChildOption.FindChildrenRecursively) As QObject
+
+        ' TODO StaticMetaObject returns Null reference exception
+        Dim meta As QMetaObject = QPushButton.StaticMetaObject
+        'Dim meta As QMetaObject = GetType(T).GetProperty("StaticMetaObject").GetValue(Nothing, Nothing)
+        
+        Dim ret = QWidget.Qt_qFindChild_helper(parent,name,meta,options)
+        Return ret
+    End Function
+
+
+
+    Public shared Function findChildren(of T As QObject)(parent As QObject, name As string, _
+            Optional options As Qt.FindChildOption = Qt.FindChildOption.FindChildrenRecursively) As List(Of T)
+
+        Dim ret As New List(Of T)
+
+        ' TODO need to find a way of passing types into QList, then getting the QMetaObject from it
+        Dim x As New QList()
+
+        Dim meta As QMetaObject = QPushButton.StaticMetaObject
+        'Dim meta As QMetaObject = GetType(T).GetProperty("StaticMetaObject").GetValue(Nothing, Nothing)
+        
+        Dim test1 = QWidget.Qt_qFindChild_helper(parent,name,meta,options)
+
+        Return ret
+    End Function
+
+
 
 End Class
